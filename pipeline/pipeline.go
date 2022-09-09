513de96cf76068d755612fbd38c23f26e4504d55
Ianmohc <ian@mohc.net>
2022-09-09 15:00:37 +0100

    refactor: renaming to make generalise the variables a little better

--- pipeline/pipeline.go
+++ pipeline/pipeline.go
@@ -19,37 +19,37 @@ func (pipeline Pipeline[T]) Run(elements []T) error {
 }
 
 // Stage represents a specific stage in a pipeline
-type Stage[T any] func(collection []T) ([]T, error)
+type Stage[T any] func(elements []T) ([]T, error)
 
 // SideEffect is used when a statement is performing a side effect action such as calling an API
-type SideEffect[T any] func(data T) error
+type SideEffect[T any] func(element T) error
 
 // Predicate is used when a statement is to be checked against fo either a true or false value
-type Predicate[T any] func(data T) bool
+type Predicate[T any] func(element T) bool
 
 // Do is the main pipeline stage for processing the different stages of the pipeline
 func Do[T any](effect SideEffect[T]) Stage[T] {
-	return func(collection []T) ([]T, error) {
-		for _, data := range collection {
-			err := effect(data)
+	return func(elements []T) ([]T, error) {
+		for _, element := range elements {
+			err := effect(element)
 			if err != nil {
 				return []T{}, err
 			}
 		}
 
-		return collection, nil
+		return elements, nil
 	}
 }
 
 // Filter is used for filtering out unwanted elements from the pipeline using the `Predicate`
 func Filter[T any](predicate Predicate[T]) Stage[T] {
-	return func(collection []T) ([]T, error) {
-		passedData := make([]T, 0, len(collection))
-		for _, data := range collection {
-			if predicate(data) {
-				passedData = append(passedData, data)
+	return func(elements []T) ([]T, error) {
+		passedElement := make([]T, 0, len(elements))
+		for _, element := range elements {
+			if predicate(element) {
+				passedElement = append(passedElement, element)
 			}
 		}
-		return passedData, nil
+		return passedElement, nil
 	}
 }
